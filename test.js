7efc5b4874472d73cd3fbe979bdbf970bf6c2a6c
Daniel Walter <danielwalterpand@gmail.com>
2017-05-02 20:06:33 -0300

    Alteração_Correção dos testes

--- test.js
+++ test.js
@@ -1,23 +1,13 @@
-const lab = require('lab');
-const lab = exports.lab = lab.script();
+const Lab = require('lab');
+const lab = exports.lab = Lab.script();
 
 const { expect } = require('code');
 
-const { calcularTotalItem } = require('./index.js');
+const { calculaTotalItem } = require('./calculaTotalItem');
 
-lab.experiment('Quando não informa parâmetros', () => {
-  lab.test('Deve resultado vazio', (done) => {
-    expect(calcularTotalItem()).to.equal({resultado: []});
-    expect(calcularTotalItem([])).to.equal({resultado: []});
-  });
-});
-
-//Implementação dos nomes do produto
 lab.experiment('quando informa ID', () => {
-lab.test('deve retornar com o nome e o valor do produto ', (done) => {
-    const lista = ['Maçã', 'Banana', 'Limão', 'Abacaxi'];
-    const retorno = { resultado:[{Id: 1,produto: 'Maçã', valor unitário: 4}, {Id: 2, produto: 'Banana', valor unitário: 3}, {Id: 3, produto: 'Limão', valor unitário: 5}, {Id: 6, produto: 'Abacaxi', valor unitário: 10}]};
-    expect(calcularTotalItem(lista)).to.equal(retorno);
+  lab.test('deve retornar com o nome e o valor do produto ', (done) => {
+    expect((1,5)).to.equal({ Id: 1, Nome: 'Maça', valorUnitário: 50 });
     done();
   });
 });
