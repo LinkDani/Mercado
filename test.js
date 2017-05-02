f2899ee6b0db14e12145ee3cf9d1b42339ad2f0a
Daniel Walter <danielwalterpand@gmail.com>
2017-05-02 20:49:39 -0300

    Implementação do teste tabela

--- test.js
+++ test.js
@@ -4,10 +4,10 @@ const lab = exports.lab = Lab.script();
 const { expect } = require('code');
 
 const { calculaTotalItem } = require('./calculaTotalItem');
-
+ //testando somente um valorUnitário
 lab.experiment('quando informa ID', () => {
   lab.test('deve retornar com o nome e o valor do produto ', (done) => {
-    expect(buscaProduto(1,5)).to.equal({ id: 1, Nome: 'Maça', valorUnitário: 50 });
+    expect(buscaProduto(1,5)).to.equal({ id: 1, Nome: 'Maçã', valorUnitário: 50 });
     done();
   });
 });
@@ -15,6 +15,18 @@ lab.experiment('quando informa ID', () => {
 lab.experiment('quando não-informa ID', () => {
   lab.test('deve retornar com: valor não econtrado ', (done) => {
     expect(buscaProduto(7,5)).to.equal('Produto não encontrado');
+    done();
+  });
+});
+
+lab.experiment('quando informa opção para exibir atributos de valorUnitário e produto', () => {
+  lab.test('deve retornar com o vencedor', (done) => {
+     const opcoes = ['Maçã', 'Banana', 'Açai'];
+     const retorno = { resultado: [
+       {id: 1, valorUnitário: 10}
+       {id: 2, valorUnitário: 4}
+       {id: 3, valorUnitário: 6}
+       ]};
     done();
   });
 });
