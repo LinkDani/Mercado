2ab4e62321524a963010a62c3badc8187da2a944
Daniel Walter <danielwalterpand@gmail.com>
2017-05-02 20:40:51 -0300

    Implementação de um novo teste

--- test.js
+++ test.js
@@ -7,7 +7,14 @@ const { calculaTotalItem } = require('./calculaTotalItem');
 
 lab.experiment('quando informa ID', () => {
   lab.test('deve retornar com o nome e o valor do produto ', (done) => {
-    expect((1,5)).to.equal({ Id: 1, Nome: 'Maça', valorUnitário: 50 });
+    expect(buscaProduto(1,5)).to.equal({ id: 1, Nome: 'Maça', valorUnitário: 50 });
+    done();
+  });
+});
+
+lab.experiment('quando não-informa ID', () => {
+  lab.test('deve retornar com: valor não econtrado ', (done) => {
+    expect(buscaProduto(7,5)).to.equal('Produto não encontrado');
     done();
   });
 });
